10235759bdd53bacfa3996642102a9efc655e6f8
sonvo0302 <57646470+sonvo0302@users.noreply.github.com>
2020-08-16 10:04:31 +0700

    Initial Author Routes

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var dotenv = require('dotenv').config()
 const express=require('express');
 const app=express();
 const expressLayouts=require('express-ejs-layouts');
+const bodyParser =require('body-parser');
 
-const indexRouter=require('./routes/index.js');
+
+
+const indexRouter=require('./routes/index');
+const authorRouter=require('./routes/authors');
 
 app.set('view engine','ejs');
 app.set('views',__dirname+"/views");
@@ -12,10 +16,11 @@ app.set('views',__dirname+"/views");
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 //connect mongoose
-
+require('dotenv').config()
 const mongoose = require('mongoose');
+// const { config } = require('dotenv/types');
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db=mongoose.connection;
 db.on('error',eror=>console.error(error));
@@ -27,10 +32,11 @@ db.once('open',()=>console.log('Connected Successfully'));
 // });
 
 
+
+app.use('/',indexRouter);
+app.use('/authors',authorRouter);
+
 const PORT = 3000;
 app.listen(PORT,()=>{
 console.log(`Server Running on port ${PORT}`);
 });
-
-
-app.use('/',indexRouter);
