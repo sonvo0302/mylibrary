f9cae80cfa29a280163280dcc9f6498bebda6ad1
sonvo0302 <57646470+sonvo0302@users.noreply.github.com>
2020-08-17 11:04:01 +0700

    Finish Authors Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,62 +1,111 @@
-const express=require('express');
-const router =express.Router();
-const Author =require('../models/authors');
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/authors');
+const Book = require('../models/book');
 const { response } = require('express');
 
 //All Authors Route
-router.get('/', async(req,res)=>{
-    let searchOptions={}
-    if(req.query.name != null && req.query.name != ''){
-        searchOptions.name = new RegExp(req.query.name,'i');
+router.get('/', async(req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name != '') {
+        searchOptions.name = new RegExp(req.query.name, 'i');
     }
-    
+
     try {
         const authors = await Author.find(searchOptions);
-        res.render('authors/index',{ authors : authors , searchOptions:req.query});
+        res.render('authors/index', { authors: authors, searchOptions: req.query });
     } catch {
-        res.redirect('/')        
+        res.redirect('/')
     }
 });
 //New author route
-router.get('/new',(req,res)=>{
-    res.render('authors/new',{ author : new Author()});
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
 })
-//Create Author Route
-router.post('/',async(req,res)=>{
-    const author=new Author({
-        name:req.body.name
-    })
-    // try {
-    //     const newAuthor = await author.save()
-    //     res.redirect(`authors`);
-    // } catch {
-    //     res.render('authors/new',{
-    //         author:author,
-    //         errorMessage : 'Error creating Author'
-    //     })
-    // }
-        author.save((err,newAuthor)=>{
-    if(err){
-        res.render('authors/new',{
-            author:author,
-            errorMessage : 'Error creating Author'
-        })
-    }else{
-        res.redirect(`authors`);
-    }
-    })
-})
-//     author.save((err,newAuthor)=>{
-//     if(err){
-//         res.render('authors/new',{
-//             author:author,
-//             errorMessage : 'Error creating Author'
-//         })
-//     }else{
-//         res.redirect(`authors`);
-//     }
-//     })
-//     res.send(req.body.name);
-// })
 
-module.exports=router;
+//Create Author Route
+router.post('/', async(req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect(`/authors/${newAuthor.id}`);
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render('authors/new', {
+    //             author: author,
+    //             errorMessage: 'Error creating Author'
+    //         })
+    //     } else {
+    //         res.redirect(`authors/${author.id}`);
+    //     }
+    // })
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({ author: author.id }).limit(6).exec();
+        res.render('authors/show', {
+            author: author,
+            booksByAuthor: books
+        })
+    } catch (err) {
+        res.redirect('/')
+    }
+});
+
+router.get('/:id/edit', async(req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        res.render('authors/edit', { author: author })
+    } catch {
+        res.redirect('/authors')
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name;
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect(`/authors`)
+    } catch {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+
+        }
+    }
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 var dotenv = require('dotenv').config()
 
-const express=require('express');
-const app=express();
-const expressLayouts=require('express-ejs-layouts');
-const bodyParser =require('body-parser');
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
-const indexRouter=require('./routes/index');
-const authorRouter=require('./routes/authors');
-const bookRouter=require('./routes/books');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + "/views");
 
-app.set('view engine','ejs');
-app.set('views',__dirname+"/views");
-
-app.set('layout','layouts/layout');
+app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+app.use(methodOverride('_method'));
 //connect mongoose
 require('dotenv').config()
 const mongoose = require('mongoose');
 // const { config } = require('dotenv/types');
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
-const db=mongoose.connection;
-db.on('error',eror=>console.error(error));
-db.once('open',()=>console.log('Connected Successfully'));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection;
+db.on('error', eror => console.error(error));
+db.once('open', () => console.log('Connected Successfully'));
 
 // mongoose.connect(`mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@ds241658.mlab.com:41658/test_db`,(err)=>{
 // if(err) throw err;
@@ -34,11 +35,12 @@ db.once('open',()=>console.log('Connected Successfully'));
 
 
 
-app.use('/',indexRouter);
-app.use('/authors',authorRouter);
-app.use('/books',bookRouter);
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
+
 
 const PORT = 3000;
-app.listen(PORT,()=>{
-console.log(`Server Running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
 });
